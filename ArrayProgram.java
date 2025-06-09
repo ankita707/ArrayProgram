1e60f23c75514021b90274f5d60e95d3f33dc651
Jyothi2005 <JyothiHudali05@gmail.com>
2025-06-09 10:23:36 +0530

    save

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -1,25 +1,34 @@
 package array;
 
 public class ArrayProgram {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+        int sum=0;
+        int min=0;
         int max=0;
         int n=5;
-        int min=0;
-        int[] scores={0,20,30,40,50};
+        int[] scores={10,20,30,40,50};
     for(int i=0;i<n;i++)
 {
-    if(min>scores[i])
+    sum+=scores[i];
+}  
+System.out.println("The sum of scores is"+sum);
+int avg=sum/5;
+System.out.println("The average of scores is"+avg);
+for(int i=0;i<n;i++)
+{
+    if(min < scores[i])
     {
-        min=scores[i];
+ min = scores[i];
     }
-    if(max<scores[i])
+    if(max>scores[i])
     {
         max=scores[i];
     }
-}  
-System.out.println("The minimum scores is"+min);
-System.out.println("The maximum scores is"+max);
+}
+System.out.println("The minimum value"+min);
+System.out.println("The maximum value"+max);
 }
 }
+
     
 
